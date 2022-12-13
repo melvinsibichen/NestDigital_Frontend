ce8751cb764cffc0828b72f061d3e7212ac7cd2e
melvinsibichen <melvinsibichen1020@gmail.com>
2022-12-13 10:26:03 +0530

    api added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
-import { AdminNavBarComponent } from './admin-nav-bar/admin-nav-bar.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { AdminNavBarComponent } from './admin-nav-bar/admin-nav-bar.component';
+
+const myRoute:Routes = [
+  {
+    path: "",
+    component: AdminLoginComponent
+  },
+  {
+    path:"addEmployee",
+    component: AddEmployeeComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminLoginComponent,
-    AdminNavBarComponent,
-    AddEmployeeComponent
+    AddEmployeeComponent,
+    AdminNavBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
