8b2cd3a3a5c355839db45d6e8b151dfd45a93a29
melvinsibichen <melvinsibichen1020@gmail.com>
2022-12-13 10:19:38 +0530

    ADMIN properties added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { AdminNavBarComponent } from './admin-nav-bar/admin-nav-bar.component';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminLoginComponent,
+    AdminNavBarComponent,
+    AddEmployeeComponent
   ],
   imports: [
     BrowserModule,
