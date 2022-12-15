94f86689541600f5d997e9cdd9d67ec13ded9073
melvinsibichen <melvinsibichen1020@gmail.com>
2022-12-15 22:11:39 +0530

    leave added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,21 @@ import { ViewEmployeeComponent } from './view-employee/view-employee.component';
 import { EditEmployeeComponent } from './edit-employee/edit-employee.component';
 import { AddSecurityComponent } from './add-security/add-security.component';
 import { HandleLeaveComponent } from './handle-leave/handle-leave.component';
+import { SecurityLoginComponent } from './security-login/security-login.component';
+import { ViewProfileComponent } from './view-profile/view-profile.component';
+import { SecurityNavComponent } from './security-nav/security-nav.component';
+import { CreateLogComponent } from './create-log/create-log.component';
+import { VisitorsLogComponent } from './visitors-log/visitors-log.component';
+import { EmployeeLoginComponent } from './employee-login/employee-login.component';
+import { EmployeeProfileComponent } from './employee-profile/employee-profile.component';
+import { EmplNavbarComponent } from './empl-navbar/empl-navbar.component';
+import { ApplyLeaveComponent } from './apply-leave/apply-leave.component';
+import { ViewEmployeeLeaveComponent } from './view-employee-leave/view-employee-leave.component';
+import { ViewEmployeeLogComponent } from './view-employee-log/view-employee-log.component';
+import { ViewSecEmpLogsComponent } from './view-sec-emp-logs/view-sec-emp-logs.component';
+import { ViewSecVisLogComponent } from './view-sec-vis-log/view-sec-vis-log.component';
+import { ViewAdminEmpLogComponent } from './view-admin-emp-log/view-admin-emp-log.component';
+import { ViewAdminVisLogComponent } from './view-admin-vis-log/view-admin-vis-log.component';
 
 const myRoute:Routes = [
   {
@@ -38,6 +53,58 @@ const myRoute:Routes = [
   {
     path:"viewLeaves",
     component:HandleLeaveComponent
+  },
+  {
+    path:"securityLogin",
+    component:SecurityLoginComponent
+  },
+  {
+    path:"viewSecurityProfile",
+    component: ViewProfileComponent
+  },
+  {
+    path:"employeeLog",
+    component: CreateLogComponent
+  },
+  {
+    path:"visitorLog",
+    component: VisitorsLogComponent
+  },
+  {
+    path:"employeeLogin",
+    component: EmployeeLoginComponent
+  },
+  {
+    path:"employeeProfile",
+    component: EmployeeProfileComponent
+  },
+  {
+    path:"applyLeave",
+    component: ApplyLeaveComponent
+  },  
+  {
+    path:"viewEmployeeLeave",
+    component: ViewEmployeeLeaveComponent
+  },
+  {
+    path:"viewEmployeeLog",
+    component: ViewEmployeeLogComponent
+  },
+  {
+    path:"viewSecEmpLog",
+    component: ViewSecEmpLogsComponent
+  },
+  {
+    path:"viewSecVisLog",
+    component: ViewSecVisLogComponent
+  },
+  {
+    path:"viewAdminEmpLog",
+    component: ViewAdminEmpLogComponent
+  },
+  {
+    path:"viewAdminVisLog",
+    component: ViewAdminVisLogComponent
   }
 ]
 
@@ -50,7 +117,22 @@ const myRoute:Routes = [
     ViewEmployeeComponent,
     EditEmployeeComponent,
     AddSecurityComponent,
-    HandleLeaveComponent
+    HandleLeaveComponent,
+    SecurityLoginComponent,
+    ViewProfileComponent,
+    SecurityNavComponent,
+    CreateLogComponent,
+    VisitorsLogComponent,
+    EmployeeLoginComponent,
+    EmployeeProfileComponent,
+    EmplNavbarComponent,
+    ApplyLeaveComponent,
+    ViewEmployeeLeaveComponent,
+    ViewEmployeeLogComponent,
+    ViewSecEmpLogsComponent,
+    ViewSecVisLogComponent,
+    ViewAdminEmpLogComponent,
+    ViewAdminVisLogComponent,
   ],
   imports: [
     BrowserModule,
